57acd6e4e1f9a2337446df124e2e3923204aa824
artfalcao <arthurmonteirofalcao1995@gmail.com>
2025-01-09 20:48:38 -0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import mongoose from "mongoose";
+import userRoutes from "./routes/userRoutes";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+
+// MongoDB connection
+const mongoURI = process.env.MONGO_URI || "mongodb://localhost:27017/mydatabase";
+// mongoose
+//   .connect(mongoURI, {
+//     dbName: "mydatabase",
+//   })
+//   .then(() => console.log("MongoDB connected"))
+//   .catch((error) => console.error("MongoDB connection error:", error));
+
+// Routes
+app.use("/users", userRoutes);
+
+// Start server
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
