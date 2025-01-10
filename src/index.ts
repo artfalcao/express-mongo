1cabb944dedd154647edfef165b7fac939cfb325
artfalcao <arthurmonteirofalcao1995@gmail.com>
2025-01-09 22:39:25 -0300

    fix: connect mongo

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,19 @@ import mongoose from "mongoose";
 import userRoutes from "./routes/userRoutes";
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 
 // Middleware
 app.use(express.json());
 
 // MongoDB connection
 const mongoURI = process.env.MONGO_URI || "mongodb://localhost:27017/alura-books";
-// mongoose
-//   .connect(mongoURI, {
-//     dbName: "mydatabase",
-//   })
-//   .then(() => console.log("MongoDB connected"))
-//   .catch((error) => console.error("MongoDB connection error:", error));
+mongoose
+  .connect(mongoURI, {
+    dbName: "mydatabase",
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((error) => console.error("MongoDB connection error:", error));
 
 // Routes
 app.use("/users", userRoutes);
