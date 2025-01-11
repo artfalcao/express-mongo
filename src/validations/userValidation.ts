be2670255d705e5894a91f119419783b5e80daad
artfalcao <arthurmonteirofalcao1995@gmail.com>
2025-01-11 12:08:58 -0300

    fix: userValidation messages

--- src/validations/userValidation.ts
+++ src/validations/userValidation.ts
@@ -3,5 +3,5 @@ import { body } from 'express-validator';
 export const userValidationRules = [
   body('name').notEmpty().withMessage('Name is required'),
   body('email').notEmpty().withMessage('Email is required'),
-  body('phone').notEmpty().withMessage('Phone must be a boolean'),
+  body('phone').notEmpty().withMessage('Phone is required'),
 ];
